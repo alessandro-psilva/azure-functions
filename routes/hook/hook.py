7f2a090a9841ab8b610aae989cabde6fe2a402f9
Alessandro Silva <alessandro-psilva@outlook.com>
2023-11-09 23:17:00 -0300

    ajuste

--- routes/hook/hook.py
+++ routes/hook/hook.py
@@ -13,19 +13,13 @@ bp = func.Blueprint()
 def hook(req: func.HttpRequest) -> func.HttpResponse:
     logging.info("[hook] HTTP trigger function processed a request.")
     req_body = req.get_json()
-    hook = req_body.get("hook", "")
 
-    if hook:
-        logging.info(f"Req Body: {req_body}")
-        response = requests.post(
-            url="https://webhook.site/912436ea-f2b4-4836-9086-9cfef7259a3f",
-            json=req_body,
-        )
+    logging.info(f"Req Body: {req_body}")
+    response = requests.post(
+        url="https://webhook.site/912436ea-f2b4-4836-9086-9cfef7259a3f",
+        json=req_body,
+    )
 
-        return func.HttpResponse(
-            json.dumps({"ResponseHook": response.text}), status_code=200
-        )
-    else:
-        return func.HttpResponse(
-            json.dumps({"RequestHook": "Input Null"}), status_code=400
-        )
+    return func.HttpResponse(
+        json.dumps({"ResponseHook": response.text}), status_code=200
+    )
